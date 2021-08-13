f6eb8f31f009db08a128aeaca32d60c585a16dcf
SlaYpoint <codersaysady@gmail.com>
2021-08-13 14:39:16 +0530

    added logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import { Footer } from "./Footer";
 
-function App() {
+export default function App() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0.00);
+  const [qty, setQty] = useState(0);
+  const [curPrice, setCurPrice] = useState(0.00);
+  const [output, setOutput] = useState("");
+
+  const stockUrl = (name) => {
+    let symbol = name.toUpperCase();
+    return `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${process.env.REACT_APP_API_TOKEN}`;
+  };
+
+  const errorHandler = (err) => {
+    console.log("error has occured",err);
+    alert("some issue with the server: try after sometime.");
+  };
+
+  const fetchData = () => {
+    fetch(stockUrl(name))
+          .then((res) => res.json())
+          .then((data) => {
+              setCurPrice(data.c);
+          })
+      .catch(errorHandler);
+  }
+
+  const calculate = () => {
+      
+      if (!isNaN(price) && !isNaN(qty)) {
+        fetchData();
+
+        if (price > 0 && qty > 0 && curPrice > 0) {
+              let cp = qty * price;
+              let sp = qty * curPrice;
+              if (cp > sp) {
+                  const loss = (cp - sp).toFixed(2);
+                  const lossPer = (((cp - sp) * 100) / cp).toFixed(2);
+                  setOutput(
+                    `Suffered a ${lossPer}% loss. You total loss is $${loss}.`
+                  );
+              }
+              else {
+                  const profit = (sp - cp).toFixed(2);
+                  const profitPer = (((sp - cp) * 100) / cp).toFixed(2);
+                  setOutput(
+                    `Yay! a ${profitPer}% pay off. You total profit is $${profit}.`
+                  );
+              }
+          } else {
+              setOutput("Please give valid input(only numbers > 0)");
+          }
+      } else {
+        setOutput("Please give valid input(only numbers > 0)");
+      }
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <h1>
+            pay<span className="text--focus">Off</span>
+          </h1>
+        </header>
+
+        <section className="data">
+          <input
+            type="text"
+            id="stock"
+            className="input__box"
+            placeholder="Stock (e.g: AAPL, TSLA)"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            id="cp"
+            className="input__box"
+            placeholder="Purchased for ?"
+            onChange={(e) => setPrice(Number(e.target.value))}
+          />
+          <input
+            type="text"
+            id="qty"
+            className="input__box"
+            placeholder="How many ?"
+            onChange={(e) => setQty(Number(e.target.value))}
+          />
+        </section>
+
+        <button className="btn primary__btn" onClick={calculate}>
+          paid off?
+        </button>
+
+        <div className="output">
+          <h4>{output}</h4>
+        </div>
+
+        <section className="help">
+          <p>
+            <strong>Note </strong>
+          </p>
+          <p>
+            Profit/loss are calculated on current stock prices in the US market.
+            Might not work for Indian stocks
+          </p>
+        </section>
+      </div>
+
+      <Footer />
     </div>
   );
 }
-
-export default App;
